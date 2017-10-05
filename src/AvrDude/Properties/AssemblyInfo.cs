1f32b136b48f5de1707f16110025974bcbbc7ade
Robert Snyder <immeraufdemhund@hotmail.com>
2017-10-05 14:40:33 -0400

    Started work on porting avrdude to C#

---
+++ src/AvrDude/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("AvrDude")]
+[assembly: AssemblyProduct("AvrDude")]
+[assembly: AssemblyCopyright("Copyright © Robert Snyder 2017")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
